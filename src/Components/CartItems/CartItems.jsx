d62e90e69ee2d176429e39b3a64d038db39a7b67
Bildad Ronoh <Bildadronoh@gmail.com>
2024-03-07 23:26:11 +0300

    changes

---
+++ src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './CartItems.css'
+
+const CartItems = () => {
+    const {all_product,cartItems,removeFromCart} = useContext(ShopContext);
+  return (
+    <div className='cartitems'>
+        <div className="cartitems-format-main">
+            <p>Products</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+            <p>Remove</p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default CartItems
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom'
 import { ShopContext } from '../Context/ShopContext'
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
 import './Product.css'
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -13,6 +15,8 @@ const Product = () => {
     <div className='main-content'>
        <Breadcrum product = {product} />
        <ProductDisplay product = {product} />
+       <DescriptionBox />
+       <RelatedProducts/>
     </div>
   )
 }
