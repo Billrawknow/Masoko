16a47d585f13f95124ce932250b744de1c75683a
Bildad Ronoh <Bildadronoh@gmail.com>
2024-04-07 22:24:03 +0300

    create API for the products

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const Product = mongoose.model("Product",{
         type:String,
         required:true,
     },
-    nw_pricw:{
+    new_price:{
         type:Number,
         required:true,
     },
@@ -80,10 +80,55 @@ const Product = mongoose.model("Product",{
 
 })
 
-app.post('/addproduct',async()=>{
-    id:req.body.id
+app.post('/addproduct',async(req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if (products.length>0)
+    {
+     let last_product_array = products.slice(-1);
+     let last_product = last_product_array[0];  
+     id = last_product.id+1;
+    }
+    else{
+        id =1;
+    }
+
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    
+    })
 })
 
+// Creating API For deleting Products
+
+app.post('/removeproduct',async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+//Creating API for getting all products
+app.get('/allproducts',async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All Productd Fetched")
+    res.send(products);
+})
 app.listen(port,(error)=>{
     if (!error){
         console.log("Server Running on Port "+port);
